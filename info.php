fc6d8be9173ac90990d0bbd59b81c724bb701b91
Hirotoshi Uchida <uho02741358@gmail.com>
2024-07-17 22:43:39 +0900

    Update info.php
    Signed-off-by: Hirotoshi Uchida <uho02741358@gmail.com>

--- info.php
+++ info.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" , content="IE=edge" />
     <meta http-equiv="Content-Style-Type" , content="text/css" />
     <link rel="stylesheet/css" type="text/css" href="./style.css">
+    <link rel="stylesheet/sass" type="text/sass" href="./style.sass">
+    <link rel="stylesheet/scss" type="text/scss" href="./style.scss">
+    <link rel="stylesheet/less" type="text/less" href="./style.less">
     <script type="text/javascript" src="./server.js">
     </script>
     <title>PHP Info</title>
