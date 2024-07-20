3a56dd0711d01e134f34699441b2a203a9a7454b
Hirotoshi Uchida <uho02741358@gmail.com>
2024-07-20 21:57:49 +0900

    Update index.php
    Signed-off-by: Hirotoshi Uchida <uho02741358@gmail.com>

--- index.php
+++ index.php
@@ -96,7 +96,7 @@
   })();
   </script>
   <?php
-  $GLOBALS['title'] = 'Gradations';
+  $GLOBALS['title'] = 'Noise';
   $GLOBALS['subtitle'] = 'by PHP';
   $GLOBALS['h1'] = rand(36, 40);
   $GLOBALS['h2'] = rand(31, 35);
