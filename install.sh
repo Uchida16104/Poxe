5c791c9bbe4cfee7c890a4dc8feb35aef8e1f0e7
Hirotoshi Uchida <uho02741358@gmail.com>
2024-07-21 20:23:40 +0900

    Update install.sh
    Signed-off-by: Hirotoshi Uchida <uho02741358@gmail.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 apt-get update
-apt-get install -y apache2 php libapache2-mod-php
+apt-get remove --purge apache2
+apt-get update
+apt-get install -y apache2 apache2-dev php libapache2-mod-php
+chown -R www-data:www-data /var/www
+chmod -R 755 /var/www
 a2enmod rewrite
 service apache2 restart
+apache2ctl -D FOREGROUND
