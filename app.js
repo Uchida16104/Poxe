eb685b802c5b1be1ab28f92fe028323b2c25785d
Hirotoshi Uchida <uho02741358@gmail.com>
2024-07-24 21:57:37 +0900

    Update app.js
    Signed-off-by: Hirotoshi Uchida <uho02741358@gmail.com>

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     request.onreadystatechange = function() {
         if (request.readyState === 4 && request.status === 200) {
             let jsonData = JSON.parse(request.responseText);            
-            console.log(JSON.stringify(jsonData));
+            console.log(JSON.stringify(jsonData, null , "\t"));
         }
     };
 });
