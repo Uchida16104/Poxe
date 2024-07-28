baae2fe5a7a7442bbaeaf889540aa7b59d55e2ef
Hirotoshi Uchida <uho02741358@gmail.com>
2024-07-28 13:45:28 +0900

    Update connection.php
    Signed-off-by: Hirotoshi Uchida <uho02741358@gmail.com>

--- connection.php
+++ connection.php
@@ -1,32 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset='utf-8' />
-  <meta name="viewport" , content="width=device-width, initial-scale=1.0" />
-  <meta http-equiv="X-UA-Compatible" , content="IE=edge" />
-  <meta http-equiv="Content-Style-Type" , content="text/css" />
-  <?php
-  $GLOBALS['title'] = 'Connection';
-  $GLOBALS['error'] = 'Connection failed: ';
-  $GLOBALS['success'] = 'Connected successfully';
-  ?>
-  <title><?php echo $GLOBALS['title']; ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" , content="IE=edge" />
+    <meta http-equiv="Content-Style-Type" , content="text/css" />
+    <link rel="stylesheet/css" type="text/css" href="./style.css">
+    <link rel="stylesheet/sass" type="text/sass" href="./style.sass">
+    <link rel="stylesheet/scss" type="text/scss" href="./style.scss">
+    <link rel="stylesheet/less" type="text/less" href="./style.less">
+    <script type="text/javascript" src="./server.js">
+    </script>
+    <script type="text/javascript" src="./script.js">
+    </script>
+    <title>Connection Result</title>
 </head>
 <body>
-<h1><?php echo $GLOBALS['title']; ?></h1>
-<h2>
-<?php
-　$servername = getenv('DB_SERVER');
-　$port = getenv('DB_PORT');
-　$username = getenv('DB_USERNAME');
-　$password = getenv('DB_PASSWORD');
-　$dbname = getenv('DB_DATABASE');
-　$conn = new mysqli($servername, $username, $password, $dbname, $port);
-　if ($conn->connect_error) {
-  　  echo die($GLOBALS['error'] . $conn->connect_error);
-　}
-　echo $GLOBALS['success'];
-?>
-</h2>
+    <?php
+    $GLOBALS['title'] = 'Connection Result';
+    $GLOBALS['error'] = 'Connection failed: ';
+    $GLOBALS['success'] = 'Connected successfully';
+    ?>
+    <h1><?php echo $GLOBALS['title']; ?></h1>
+    <h2>
+    <?php
+　  $servername = getenv('DB_SERVER');
+　  $port = getenv('DB_PORT');
+　  $username = getenv('DB_USERNAME');
+　  $password = getenv('DB_PASSWORD');
+　  $dbname = getenv('DB_DATABASE');
+　  $conn = new mysqli($servername, $username, $password, $dbname, $port);
+  　if ($conn->connect_error) {
+    　  echo die($GLOBALS['error'] . $conn->connect_error);
+  　}
+  　echo $GLOBALS['success'];
+    ?>
+   </h2>
 </body>
 </html>
