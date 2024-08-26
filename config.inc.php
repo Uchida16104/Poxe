b8d77c594d8ef05b6e61cb3199236d73be7a07b6
Hirotoshi Uchida <uho02741358@gmail.com>
2024-08-26 23:14:24 +0900

    Create config.inc.php
    Signed-off-by: Hirotoshi Uchida <uho02741358@gmail.com>

---
+++ config.inc.php
@@ -0,0 +1,7 @@
+<?php
+$cfg['Servers'][1]['host'] = 'mysql-uho02741358.e.aivencloud.com';
+$cfg['Servers'][1]['port'] = '27750';
+$cfg['Servers'][1]['user'] = 'avnadmin';
+$cfg['Servers'][1]['password'] = 'AVNS_VQjeR3X7mMJJXQWC8nL';
+$cfg['Servers'][1]['auth_type'] = 'config';
+?>
