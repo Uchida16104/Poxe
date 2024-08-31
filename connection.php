4a802f8f9635cfc6362f9affcec1bbfd60244678
Hirotoshi Uchida <uho02741358@gmail.com>
2024-08-31 13:48:18 +0900

    Update connection.php
    Signed-off-by: Hirotoshi Uchida <uho02741358@gmail.com>

--- connection.php
+++ connection.php
@@ -21,7 +21,7 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    require 'vendor/autoload.php';
+    //require 'vendor/autoload.php';
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
     $host = 'mysql-uho02741358.e.aivencloud.com';
