9c7344083dd1dc3c5105ba6e4330f9f176a90913
Hirotoshi Uchida <uho02741358@gmail.com>
2024-08-31 14:51:57 +0900

    Update db.sql
    Signed-off-by: Hirotoshi Uchida <uho02741358@gmail.com>

--- db.sql
+++ db.sql
@@ -1,14 +1,2 @@
-CREATE DATABASE glitch;
-USE glitch;
-create table dreams(
-   dream VARCHAR(100) NOT NULL,
-   epoch int(30) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
-);
-INSERT INTO users (name, email) VALUES
-('Alice', 'alice@example.com'),
-('Bob', 'bob@example.com');
+ALTER USER 'avnadmin'@'%' IDENTIFIED WITH mysql_native_password BY 'AVNS_VQjeR3X7mMJJXQWC8nL';
+FLUSH PRIVILEGES;
