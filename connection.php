04f5d5c40564c9ad8c78b160a7f2355e66db0b13
Hirotoshi Uchida <uho02741358@gmail.com>
2024-09-06 18:03:49 +0900

    Update connection.php
    Signed-off-by: Hirotoshi Uchida <uho02741358@gmail.com>

--- connection.php
+++ connection.php
@@ -35,6 +35,8 @@
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::MYSQL_ATTR_SSL_CA => 'ca.pem',
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
     ];
     try {
         $pdo = new PDO($dsn, $user, $pass, $options);
