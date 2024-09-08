169f97daec09c0f03fbbe58e1d4bab1f72ce5aa4
Hirotoshi Uchida <uho02741358@gmail.com>
2024-09-08 21:53:06 +0900

    Update connection.php
    Signed-off-by: Hirotoshi Uchida <uho02741358@gmail.com>

--- connection.php
+++ connection.php
@@ -24,13 +24,7 @@
     //require 'vendor/autoload.php';
     //$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     //$dotenv->load();
-    $host = 'mysql-uho02741358.e.aivencloud.com';
-    $db = 'defaultdb';
-    $user = 'avnadmin';
-    $pass = 'AVNS_VQjeR3X7mMJJXQWC8nL';
-    $port = '27750';
-    $charset = 'utf8mb4';
-    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+    $dsn = "mysql:host=mysql-uho02741358.e.aivencloud.com;port=27750;dbname=defaultdb;charset=utf8mb4";
     $options = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -39,7 +33,7 @@
         PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
     ];
     try {
-        $pdo = new PDO($dsn, $user, $pass, $options);
+        $pdo = new PDO("$dsn", 'avnadmin', 'AVNS_VQjeR3X7mMJJXQWC8nL', "$options");
         echo "Connected to the Aiven MySQL database successfully!";
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $stmt = $pdo->query('SOURCE ./db.sql; SHOW DATABASES;');
