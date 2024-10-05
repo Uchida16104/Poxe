342c90d66061112147a2106d6593d04202cedbd2
Hirotoshi Uchida <uho02741358@gmail.com>
2024-10-05 14:12:53 +0900

    Update index.php
    Signed-off-by: Hirotoshi Uchida <uho02741358@gmail.com>

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     gtag('js', new Date());
     gtag('config', 'G-38606SS5F6');
   </script>
+  <script type="text/javascript" data-cmp-ab="1" src="https://cdn.consentmanager.net/delivery/autoblocking/31566d9c7185e.js" data-cmp-host="a.delivery.consentmanager.net" data-cmp-cdn="cdn.consentmanager.net" data-cmp-codesrc="16"></script>
   <meta charset='utf-8' />
   <meta name="viewport" , content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" , content="IE=edge" />
