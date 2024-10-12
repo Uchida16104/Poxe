6a4237cd657f85bba782fb59ec9f8d12b75e60d9
Hirotoshi Uchida <uho02741358@gmail.com>
2024-10-12 17:10:59 +0900

    Create error.php
    Signed-off-by: Hirotoshi Uchida <uho02741358@gmail.com>

---
+++ error.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-38606SS5F6"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+     
+      gtag('config', 'G-38606SS5F6');
+    </script>
+    <script type="text/javascript" data-cmp-ab="1" src="https://cdn.consentmanager.net/delivery/autoblocking/31566d9c7185e.js" data-cmp-host="a.delivery.consentmanager.net" data-cmp-cdn="cdn.consentmanager.net" data-cmp-codesrc="16"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error!</title>
+</head>
+<body>
+    <h1>Authentication failed.</h1>
+    <p>Incorrect username or password.</p>
+    <button onclick="location.href='./management.php'">Manage username and password</button>
+</body>
+</html>
